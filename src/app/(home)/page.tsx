88ff59580d15fafd76009b36fff249a671eaf8f9
Amirull Azmi <amirull.azmi@praweda.id>
2025-10-01 23:45:50 +0700

    feat: enhance mobile responsiveness for news event section and update padding in view

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                <HomeServicesView />
                <HomeSolutionView />
                <HomePublicationView />
-               {/* <HomeNewsEventView /> */}
+               <HomeNewsEventView />
                {/* <HomeDemoView /> */}
                {/* <HomeFormView /> */}
           </main>
