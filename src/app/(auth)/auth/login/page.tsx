745f0bf42eedf58cb3331989865eb2da052845d8
amirullazmi0 <amirullazmi0@gmail.com>
2025-10-25 14:23:47 +0700

    feat: refactor AuthLoginView and enhance loading state with Suspense

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import AuthLoginView from "./auth.login.view";
 
 export default function AuthLogin() {
      return (
           <div className="w-full min-h-screen flex flex-col justify-center items-center bg-gray-100">
-               <AuthLoginView />
+               <Suspense fallback={<div className="loading loading-spinner loading-lg"></div>}>
+                    <AuthLoginView />
+               </Suspense>
           </div>
      );
 }
